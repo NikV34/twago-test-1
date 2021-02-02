033c53542591e1ec69ac561a8943e22c70edef46
NikV34 <25172610+NikV34@users.noreply.github.com>
2021-02-02 18:34:58 +0200

    page without slider

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,589 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        twago-test-1
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+    <header class="header__wrapper">
+      <div class="logo__wrapper">
+        <div class="circle__wrapper">
+          <span class="circle__icon"></span>
+        </div>
+        <p class="logo__label">↘ACME</p>
       </div>
-    </div>
+      <div class="auth__wrapper">
+        <button class="auth__btn">Login</button>
+      </div>
+    </header>
+    <section>
+      <div class="h1__wrapper">
+        <p class="h1__subtitle">Coffer maroon killick bilge list driver</p>
+        <h1 class="h1__title">
+          Man-of-war Nelsons folly quarter bowsprit rutters pressgang haul wind
+          quarterdeck walk the plank knave.
+        </h1>
+      </div>
+      <ul class="block-list__wrapper">
+        <li class="block-list__item">
+          <div class="circle-title__wrapper">
+            <div class="circle__wrapper">
+              <span class="circle__icon circle-9-12"></span>
+            </div>
+            <div class="h2__wrapper">
+              <h2 class="h2__title">Resist</h2>
+            </div>
+          </div>
+          <div class="description__wrapper">
+            Movements collaborate but systems thinking commitment boots on the
+            ground. A inclusive justice segmentation ideate impact investing.
+            Scale and impact agile, social enterprise or youth B-corp.
+          </div>
+        </li>
+
+        <li class="block-list__item">
+          <div class="circle-title__wrapper">
+            <div class="circle__wrapper">
+              <span class="circle__icon circle-6-12"></span>
+            </div>
+            <div class="h2__wrapper">
+              <h2 class="h2__title">Big data</h2>
+            </div>
+          </div>
+          <div class="description__wrapper">
+            Strengthening infrastructure; collaborative cities efficient,
+            entrepreneur natural resources expose the truth collaborative
+            consumption. Humanitarian capacity building and, justice low-hanging
+            fruit society bandwidth changemaker challenges and opportunities.
+          </div>
+        </li>
+
+        <li class="block-list__item">
+          <div class="circle-title__wrapper">
+            <div class="circle__wrapper">
+              <span class="circle__icon circle-6-12-solid"></span>
+            </div>
+            <div class="h2__wrapper">
+              <h2 class="h2__title">Game-changer</h2>
+            </div>
+          </div>
+          <div class="description__wrapper">
+            Empower communities parse thought leadership, citizen-centered; mass
+            incarceration impact black lives matter deep dive the resistance.
+            Mass incarceration citizen-centered game-changer emerging;
+            collaborate global. But, strategy game-changer move.
+          </div>
+        </li>
+
+        <li class="block-list__item">
+          <div class="circle-title__wrapper">
+            <div class="circle__wrapper">
+              <span class="circle__icon circle-3-9-solid"></span>
+            </div>
+            <div class="h2__wrapper">
+              <h2 class="h2__title">Empower</h2>
+            </div>
+          </div>
+          <div class="description__wrapper">
+            Social intrapreneurship parse natural resources social entrepreneur
+            social capital social innovation agile. Thought provoking because;
+            mobilize, design thinking catalyze.
+          </div>
+        </li>
+      </ul>
+      <div class="text-block__wrapper">
+        <div class="h2__wrapper">
+          <h2 class="h2__title">
+            communities green space strengthening infrastructure dynamic
+            families thought leader.
+          </h2>
+        </div>
+        <div class="text-block__label">150 CC</div>
+      </div>
+    </section>
+    <footer>
+      <div class="footer__wrapper">
+        <div class="logo__wrapper">
+          <div class="circle__wrapper">
+            <span class="circle__icon"></span>
+          </div>
+          <p class="logo__label">↘ACME</p>
+        </div>
+        <ul class="link__list">
+          <li class="link__column">
+            <ul>
+              <li class="link__category">Product</li>
+              <li class="link__page">
+                <a
+                  href="./product/enterprise"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  class="link__label"
+                >
+                  Enterprise
+                </a>
+              </li>
+
+              <li class="link__page">
+                <a
+                  href="./product/security"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  class="link__label"
+                >
+                  Security
+                </a>
+              </li>
+
+              <li class="link__page">
+                <a
+                  href="./product/pricing"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  class="link__label"
+                >
+                  Pricing
+                </a>
+              </li>
+
+              <li class="link__page">
+                <a
+                  href="./product/demo"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  class="link__label"
+                >
+                  Demo
+                </a>
+              </li>
+            </ul>
+          </li>
+
+          <li class="link__column">
+            <ul>
+              <li class="link__category">Company</li>
+              <li class="link__page">
+                <a
+                  href="./company/about-us"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  class="link__label"
+                >
+                  About us
+                </a>
+              </li>
+
+              <li class="link__page">
+                <a
+                  href="./company/blog"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  class="link__label"
+                >
+                  Blog
+                </a>
+              </li>
+
+              <li class="link__page">
+                <a
+                  href="./company/news"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  class="link__label"
+                >
+                  News
+                </a>
+              </li>
+
+              <li class="link__page">
+                <a
+                  href="./company/careers"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  class="link__label"
+                >
+                  Careers
+                </a>
+              </li>
+            </ul>
+          </li>
+
+          <li class="link__column">
+            <ul>
+              <li class="link__category">Resources</li>
+              <li class="link__page">
+                <a
+                  href="./resources/help-center"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  class="link__label"
+                >
+                  Help Center
+                </a>
+              </li>
+              <li class="link__page">
+                <a
+                  href="./resources/api"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  class="link__label"
+                >
+                  API
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <div class="copyright__wrapper">
+        <ul class="copyright__documents">
+          <li class="copyright__document">
+            <a
+              href="./documents/status"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="link__label"
+            >
+              Status
+            </a>
+          </li>
+
+          <li class="copyright__document">
+            <a
+              href="./documents/privacy"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="link__label"
+            >
+              Privacy & Terms
+            </a>
+          </li>
+
+          <li class="copyright__document">
+            <a
+              href="./contact-us"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="link__label"
+            >
+              Contact us
+            </a>
+          </li>
+        </ul>
+        <ul class="social__list">
+          <li class="social__item">
+            <img class="social__icon" src="../assets/icons/facebook.svg" />
+          </li>
+
+          <li class="social__item">
+            <img class="social__icon" src="../assets/icons/linkedin.svg" />
+          </li>
+
+          <li class="social__item">
+            <img class="social__icon" src="../assets/icons/twitter.svg" />
+          </li>
+
+          <li class="social__item">
+            <img class="social__icon" src="../assets/icons/youtube.svg" />
+          </li>
+        </ul>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
-export default {}
 </script>
 
 <style>
+:root {
+  --dark-grey: #29394a;
+}
+
+body {
+  font-family: "Inter UI";
+  font-size: 14px;
+  font-weight: bold;
+  letter-spacing: 0;
+  line-height: 20px;
+  color: var(--dark-grey);
+}
+
+a {
+  text-decoration: none;
+  color: inherit;
+}
+
+p {
+  margin: 0;
+}
+
+ul {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+}
+
+button:focus {
+  outline: none;
+}
+
 .container {
+  max-width: 1024px;
   margin: 0 auto;
-  min-height: 100vh;
+  background-color: #ffdb04;
+  padding-right: 82px;
+  padding-left: 50px;
+}
+
+.header__wrapper {
   display: flex;
-  justify-content: center;
+  flex-direction: row;
+  justify-content: space-between;
   align-items: center;
-  text-align: center;
+  margin-left: -50px;
+  margin-right: -82px;
+  padding: 18px 40px 19px 50px;
+  box-shadow: inset 0 -3px 0 0 var(--dark-grey);
 }
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+.auth__wrapper {
+  margin-top: -5px;
+}
+
+.auth__btn {
+  font-weight: bold;
+  padding: 0;
+  border: none;
+  background-color: transparent;
+  color: var(--dark-grey);
+}
+
+.auth__btn:hover {
+  cursor: pointer;
+}
+
+.logo__wrapper {
+  display: flex;
+  align-items: center;
+}
+
+.circle__wrapper {
+  margin-right: 10px;
+}
+
+.circle__icon {
   display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+  box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  width: 36px;
+  height: 36px;
+  -moz-border-radius: 36px;
+  -webkit-border-radius: 36px;
+  border-radius: 50%;
+  border: 9px solid var(--dark-grey);
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.circle-9-12 {
+  border-left: 9px solid transparent;
+  transform: rotate(45deg);
 }
 
-.links {
-  padding-top: 15px;
+.circle-6-12 {
+  border-right: 9px solid transparent;
+  border-bottom: 9px solid transparent;
+  transform: rotate(-45deg);
+}
+
+.circle-6-12-solid {
+  background-color: var(--dark-grey);
+  width: 18px;
+  margin-right: 18px;
+  border-radius: 0;
+  border-bottom-left-radius: 36px;
+  border-top-left-radius: 36px;
+}
+
+.circle-3-9-solid {
+  background-color: var(--dark-grey);
+  height: 18px;
+  margin-top: 18px;
+  border-radius: 0;
+  border-bottom-left-radius: 36px;
+  border-bottom-right-radius: 36px;
+}
+
+.logo__label {
+  display: block;
+}
+
+.h1__subtitle {
+  font-weight: bold;
+  margin-bottom: 6px;
+}
+
+.h1__title {
+  font-size: 48px;
+  font-weight: bold;
+  line-height: 64px;
+  margin-bottom: 88px;
+}
+
+.h2__title {
+  font-size: 24px;
+  font-weight: bold;
+  line-height: 36px;
+}
+
+.block-list__wrapper {
+  display: flex;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  margin-left: 3px;
+  margin-bottom: 70px;
+}
+
+.block-list__item {
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  height: 388px;
+  border: 3px solid var(--dark-grey);
+  width: 447px;
+  padding: 46px 46px 50px 47px;
+  margin-left: -3px;
+  margin-top: -3px;
+}
+
+.circle-title__wrapper {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 24px;
+}
+
+.circle-title__wrapper .circle__wrapper {
+  margin-right: 14px;
+}
+
+.block-list__item .description__wrapper {
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 24px;
+  margin-left: 52px;
+}
+
+.block-list__item:nth-child(2) .description__wrapper {
+  margin-left: 48px;
+  margin-right: 4px;
+}
+
+.block-list__item:last-child {
+  padding-right: 20px;
+}
+
+.block-list__item:last-child .circle__wrapper {
+  margin-right: 22px;
+}
+
+.block-list__item:last-child .description__wrapper {
+  margin-left: 58px;
+}
+
+.text-block__wrapper {
+  position: relative;
+  margin-bottom: 108px;
+}
+
+.text-block__wrapper .h2__wrapper {
+  width: 391px;
+}
+
+.text-block__label {
+  position: absolute;
+  right: 0;
+  top: 0;
+  font-size: 12px;
+  line-height: 16px;
+}
+
+section {
+  padding-top: 64px;
+  margin-bottom: 458px;
+}
+
+.footer__wrapper {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+  box-shadow: inset 0 -1px 0 0 #29394a, inset 0 3px 0 0 #29394a;
+  padding-top: 40px;
+  padding-bottom: 56px;
+}
+
+.link__list {
+  display: inline-flex;
+  margin-right: 1px;
+}
+
+.link__column {
+  margin-right: 56px;
+}
+
+.link__column:last-child {
+  margin-right: 0;
+}
+
+.link__column li {
+  margin-bottom: 8px;
+}
+
+.link__column li:last-child {
+  margin-bottom: 0;
+}
+
+.link__category {
+}
+
+.link__page {
+  font-weight: 500;
+}
+
+.copyright__wrapper {
+  padding-top: 24px;
+  padding-bottom: 39px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.copyright__documents {
+  display: inline-flex;
+}
+
+.copyright__document {
+  font-size: 12px;
+  line-height: 16px;
+  margin-right: 16px;
+}
+
+.copyright__document:last-child {
+  margin-right: 0;
+}
+
+
+.social__list {
+  display: inline-flex;
+  margin-right: 2px;
+}
+
+.social__item {
+  margin-right: 16px;
+}
+
+.social__item:last-child {
+  margin-right: 0;
+}
+
+.social__icon {
+  height: 24px;
+  width: 24px;
 }
 </style>
